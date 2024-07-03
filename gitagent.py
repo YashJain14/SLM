bb60094794cafb79cf2ffa07f52e188c752c2742
Yash Jain <yashjain141103@gmail.com>
2024-07-03 11:56:37 +0530

    commit message

---
+++ gitagent.py
@@ -0,0 +1,13 @@
+# from mlx_lm import load, generate
+
+# model, tokenizer = load("YashJain/GitAI-Qwen2-0.5B-Instruct-MLX-v2")
+# response = generate(model, tokenizer, prompt='only answer me in json format do not write anything outside json, tell me about NVIDIA in json format like this {"company": "", "stock_ticker": "", "date": "", "reported_revenue": ""}',verbose=True, max_tokens=100)
+
+
+
+# print('='*50)
+# print(response)
+
+
+import os
+os.system("git add .")
